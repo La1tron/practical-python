f07235daed7fe8ca1b92d88fc7d2457691d8cd6b
赖强 <meetlq@outlook.com>
2020-05-30 18:14:53 +0800

    📦 NEW: Finish exercise 1.5: The Bouncing Ball

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+ORIGIN_HIGH = 100
+
+
+def back_up_high(origin_high):
+    return round(origin_high * 3 / 5, 4)
+
+
+for i in range(10):
+    ball_high = back_up_high(ORIGIN_HIGH)
+    print(i+1, ball_high)
+    ORIGIN_HIGH = ball_high
